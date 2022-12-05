970b0a3d5dd9a6ef19708d84d887bac710f04c59
Edgar Elizarraras <ed6ar.pkp@gmail.com>
2022-12-05 17:58:29 -0600

    First commit

---
+++ app/src/main/java/com/edgar/evaluacion/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.edgar.evaluacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.edgar.evaluacion.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    
+}
